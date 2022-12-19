c025a346e7c0489f380fc8877d12b293575d1c2d
Darksiend <logosmk2@gmail.com>
2022-12-19 16:13:15 +0200

    Work

--- backend/sections/floors/floor/floorSection.js
+++ backend/sections/floors/floor/floorSection.js
@@ -14,7 +14,9 @@ import fs from "fs";
 
 function Floor(number) {
   const tikraFloder = `./img/floorsImg/${number}/tikra/`;
+  const kirotFloder = `./img/floorsImg/${number}/kirot/`;
   let tikraimages = [];
+  let kirotimages = [];
   console.log("Floor Number", number);
   fs.readdirSync(tikraFloder).forEach((file) => {
     console.log(file);
@@ -30,7 +32,20 @@ function Floor(number) {
       );
     }
   });
-
+  fs.readdirSync(kirotFloder).forEach((file) => {
+    console.log(file);
+    if (file.split(".")[1] === "png") {
+      kirotimages.push(
+        new ImageRun({
+          data: fs.readFileSync(`./img/floorsImg/${number}/kirot/${file}`),
+          transformation: {
+            width: 500,
+            height: 150,
+          },
+        })
+      );
+    }
+  });
   this.properties = { type: SectionType.NEXT_PAGE };
   this.children = [
     new Paragraph({
@@ -47,6 +62,23 @@ function Floor(number) {
     new Paragraph({
       alignment: AlignmentType.CENTER,
       children: tikraimages,
+    }),
+
+    new Paragraph({
+      alignment: AlignmentType.CENTER,
+      children: [
+        new PageBreak(),
+        new TextRun({
+          bold: true,
+          underline: { type: UnderlineType.SINGLE },
+          size: 35,
+          text: `קירות קומת ${number}`,
+        }),
+        new Paragraph({
+          alignment: AlignmentType.CENTER,
+          children: kirotimages,
+        }),
+      ],
     }),
   ];
 }
