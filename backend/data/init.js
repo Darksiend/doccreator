234a438c295fc983799dd534919a7ba993697294
Darksiend <logosmk2@gmail.com>
2022-12-19 14:01:49 +0200

    Work

--- backend/data/init.js
+++ backend/data/init.js
@@ -7,7 +7,7 @@ const data = {
     projectName: "בחינת מיבנים",
     address: "נתניה מקדונלד 20",
     date: "12.12.2022",
-    numberOfFloors: 3,
+    numberOfFloors: 2,
     isMartef: true,
   },
 };
--- backend/sections/floors/floor/floorSection.js
+++ backend/sections/floors/floor/floorSection.js
@@ -3,23 +3,45 @@ import tikraSection from "../../tikra/tikra.js";
 const {
   Paragraph,
   SectionType,
-  Table,
-  WidthType,
-  TableRow,
   AlignmentType,
-  TableCell,
-  HeadingLevel,
-  Header,
+  UnderlineType,
   TextRun,
-  VerticalAlign,
-  PageNumber,
   ImageRun,
+  PageBreak,
 } = docx;
 import docx from "docx";
+import fs from "fs";
 
 function Floor(number) {
   this.properties = { type: SectionType.NEXT_PAGE };
-  this.children = [new Paragraph({ text: `Floor number ${number}` })];
+  this.children = [
+    new Paragraph({
+      alignment: AlignmentType.CENTER,
+      children: [
+        new TextRun({
+          bold: true,
+          underline: { type: UnderlineType.SINGLE },
+          size: 35,
+          text: `תקרת קומת ${number}`,
+        }),
+      ],
+    }),
+    new Paragraph({
+      alignment: AlignmentType.CENTER,
+      children: [
+        new ImageRun({
+          data: fs.readFileSync(
+            `./img/floorsImg/${number}/tikra/${number}.png`
+          ),
+          transformation: {
+            width: 500,
+            height: 700,
+          },
+        }),
+      ],
+    }),
+    new PageBreak(),
+  ];
 }
 
 export default Floor;
