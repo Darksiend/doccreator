255249f598e68e075d3e8b43bdc28df12a702e81
Darksiend <logosmk2@gmail.com>
2022-12-19 10:49:13 +0200

    Work

--- backend/sections/intro/introSection.js
+++ backend/sections/intro/introSection.js
@@ -13,8 +13,37 @@ const {
 } = docx;
 import docx from "docx";
 
+const introParagraph = new Paragraph({
+  alignment: AlignmentType.RIGHT,
+
+  children: [new TextRun({ bold: true, text: "מבוא", size: 25 })],
+});
+
 let introSection = {
   properties: { type: SectionType.NEXT_PAGE },
-  children: [new Paragraph({ children: [new TextRun({ text: "מבוא" })] })],
+
+  children: [
+    introParagraph,
+    new Paragraph({
+      alignment: AlignmentType.RIGHT,
+      children: [
+        new TextRun({
+          text:
+            'דו"ח זה בוצע על סמך הזמנת עבודה שנתקבלה מהמזמין. מטרת ביקורת הנדסית הינה הכנת מאגר נתונים מדגמי (בנקודות שנבדקו) על מצב קיים של הקונסטרוקציות שנבחנו.\n' +
+            "נתונים אלו הינם מקור הכרחי אשר ישמשו כחומר בידי",
+          size: 25,
+        }),
+        new TextRun({
+          bold: true,
+          text: " המתכנן",
+          size: 25,
+        }),
+        new TextRun({
+          text: " על מנת להכין את חוות\n" + "דעתו.\n",
+          size: 25,
+        }),
+      ],
+    }),
+  ],
 };
 export default introSection;
