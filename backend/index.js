c5bfaacce463ac3fb9d9f97b5fd078b1f5b80902
Darksiend <logosmk2@gmail.com>
2022-12-21 23:15:00 +0200

    Work

--- backend/index.js
+++ backend/index.js
@@ -67,7 +67,7 @@ app.get("/download", (req, res) => {
     "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
   );
   res.set("Content-Disposition", "attachment; filename=template.docx");
-  res.download("docx12345.docx");
+  res.download("docx12347.docx");
 });
 app.listen(port, (e) => {
   if (e) throw e;
