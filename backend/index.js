9ada3c84cdfa952d1f0ca99bc10c666f466c0d7b
Darksiend <logosmk2@gmail.com>
2022-12-21 19:15:34 +0200

    Work

--- backend/index.js
+++ backend/index.js
@@ -42,6 +42,11 @@ app.post("/uploadImg", (req, res) => {
 app.get("/download", (req, res) => {
   createInitPage();
   console.log("Stated creating in index.js");
+  res.set(
+    "Content-Type",
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+  );
+  res.set("Content-Disposition", "attachment; filename=template.docx");
   res.download("./docx1234.docx");
 });
 app.listen(port, (e) => {
