ba36b9d85c8a014cf631174fac9ae162a82280e8
Darksiend <logosmk2@gmail.com>
2022-12-24 14:45:08 +0200

    Work

--- frontend/src/pages/CreatingDocx.jsx
+++ frontend/src/pages/CreatingDocx.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import axios from "../axios";
 import { FileDownload } from "js-file-download";
 import "./CreatingDocx.css";
-import FloorConfigComponent from "../components/FloorConfigComponent";
 
 const CreatingDocx = () => {
   const [numberOfDocument, setNumberOfDocument] = useState("");
