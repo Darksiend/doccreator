ec7226d5d722f4721e18017c82916b3e751a63fa
Darksiend <logosmk2@gmail.com>
2022-12-25 18:35:57 +0200

    Work

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 import CreatingDocx from "./pages/CreatingDocx";
+import Header from "./components/NavBar/Header";
 
 function App() {
   return (
     <>
+      <Header />
       <CreatingDocx />
     </>
   );
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,11 +5,15 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import Header from "./components/NavBar/Header";
+import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
