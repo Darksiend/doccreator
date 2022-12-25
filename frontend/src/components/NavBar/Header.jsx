f4acf32e5f6b4f717c9936052d8a21c7315e1072
Darksiend <logosmk2@gmail.com>
2022-12-25 18:40:03 +0200

    Work

--- frontend/src/components/NavBar/Header.jsx
+++ frontend/src/components/NavBar/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Header.css";
 const Header = () => {
-  return <div className="Header"></div>;
+  return <div className="Header">החנת דוחות אוטומטית</div>;
 };
 
 export default Header;
