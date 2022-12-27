6bcddd2f57112dd6da2150aaca6e97d608071188
Darksiend <logosmk2@gmail.com>
2022-12-27 12:23:04 +0200

    Work

--- frontend/src/pages/AllDocx/AllDocx.jsx
+++ frontend/src/pages/AllDocx/AllDocx.jsx
@@ -1,13 +1,16 @@
 import React, { useEffect } from "react";
 import "./AllDocx.css";
 import axios from "../../axios";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchDocxs } from "../../redux/slices/docx";
+
 const AllDocx = () => {
+  const { docxs } = useSelector((state) => state.docxs);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchDocxs());
   }, []);
+  console.log(docxs);
   return <div className="AllDocx"></div>;
 };
 
