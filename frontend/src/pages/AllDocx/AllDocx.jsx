9659ff3d20fff607bd229b22fbc32ea9fb571c60
Darksiend <logosmk2@gmail.com>
2022-12-27 12:10:55 +0200

    Work

--- frontend/src/pages/AllDocx/AllDocx.jsx
+++ frontend/src/pages/AllDocx/AllDocx.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect } from "react";
 import "./AllDocx.css";
+import axios from "../../axios";
 const AllDocx = () => {
   useEffect(() => {
-    console.log("Eff");
+    const { data } = axios.get("/docxs");
+    console.log(data);
   }, []);
   return <div className="AllDocx"></div>;
 };
