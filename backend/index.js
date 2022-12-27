beeb89e226c44dfa895cfe3d11c1aba3429f7310
Darksiend <logosmk2@gmail.com>
2022-12-27 10:48:18 +0200

    Work

--- backend/index.js
+++ backend/index.js
@@ -86,6 +86,7 @@ app.post(
 
 app.get("/auth/me", checkAuth, UserController.getMe);
 
+app.get("/getdocx", checkAuth, DocController.getAll);
 app.post(
   "/upload/:docxnumber/:floor/:element/:dir",
   upload.single("image"),
