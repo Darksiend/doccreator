96e8109a8f538056e57bc160873b32e0b9c617ca
Darksiend <logosmk2@gmail.com>
2022-12-27 12:06:13 +0200

    Work

--- frontend/src/redux/slices/docx.js
+++ frontend/src/redux/slices/docx.js
@@ -1,13 +1,32 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "../../axios";
-const initialState = {};
+const initialState = {
+  docxs: { items: [], status: "loading" },
+};
 
-export const fetchPosts = createAsyncThunk("docx/fetchDocx", async (state) => {
+export const fetchDocxs = createAsyncThunk("docx/fetchDocx", async (state) => {
   const { data } = await axios.get("/docxs");
 
   return data;
 });
 
-const docxSlice = createSlice({ name: "docxs", initialState, reducers: {} });
+const docxSlice = createSlice({
+  name: "docxs",
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [fetchDocxs.pending]: (state) => {
+      state.docxs.status = "loading";
+    },
+    [fetchDocxs.fulfilled]: (state, action) => {
+      state.docxs.items = action.payload;
+      state.docxs.status = "loaded";
+    },
+    [fetchDocxs.rejected]: (state) => {
+      state.docxs.items = [];
+      state.docxs.status = "error";
+    },
+  },
+});
 
 export const docxsReducer = docxSlice.reducer;
