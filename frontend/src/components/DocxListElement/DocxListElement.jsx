b23e67b40c65c1447b3a178d3ab56bb96f437363
Darksiend <logosmk2@gmail.com>
2022-12-28 17:58:07 +0200

    Work

--- frontend/src/components/DocxListElement/DocxListElement.jsx
+++ frontend/src/components/DocxListElement/DocxListElement.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./DocxListElement.css";
 const DocxListElement = (props) => {
+  console.log(props);
   return <div className="DocxListElement">List Elemnt</div>;
 };
 
