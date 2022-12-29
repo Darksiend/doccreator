3d4635c80d4aaaf812885ca6252c34accd8b4887
Darksiend <logosmk2@gmail.com>
2022-12-29 11:53:02 +0200

    Work

--- frontend/src/pages/FullDocx/FullDocx.jsx
+++ frontend/src/pages/FullDocx/FullDocx.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "../../axios";
 import { useParams } from "react-router-dom";
-import "./CreatingDocx.css";
+import "./FullDocx.css";
 
 const FullDocx = () => {
   const [numberOfDocument, setNumberOfDocument] = useState("");
