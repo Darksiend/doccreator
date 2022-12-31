7a69830ace501d324ae2d0bafbc88af85676518c
Darksiend <logosmk2@gmail.com>
2022-12-31 20:13:57 +0200

    Work

--- backend/docx/sections/mainPhotos/mainPhotosSection.js
+++ backend/docx/sections/mainPhotos/mainPhotosSection.js
@@ -9,50 +9,50 @@ const {
   TextRun,
 } = docx;
 import docx from "docx";
-import data from "../../../data/init.js";
-let mainPhotos = [];
-if (!fs.existsSync(`docxData/${data.init.numberOfDocument}/mainPhotos`))
-  fs.mkdirSync(`docxData/${data.init.numberOfDocument}/mainPhotos`, {
-    recursive: true,
-  });
-fs.readdirSync(`docxData/${data.init.numberOfDocument}/mainPhotos`).forEach(
-  (file) => {
-    if (file.split(".")[1] === "png") {
-      mainPhotos.push(
-        new ImageRun({
-          data: fs.readFileSync(
-            `docxData/${data.init.numberOfDocument}/mainPhotos/${file}`
-          ),
-          transformation: {
-            width: 500,
-            height: 150,
-          },
-        })
-      );
-    }
-  }
-);
-console.log(mainPhotos);
-let mainPhotosSection = {
-  properties: { type: SectionType.NEXT_PAGE },
-  children: [
-    new Paragraph({
-      alignment: AlignmentType.CENTER,
-      spacing: { before: 200 },
-      children: [
-        new TextRun({
-          size: 35,
-          underline: { type: UnderlineType.SINGLE },
-          text: "תמונות",
-        }),
-      ],
-    }),
-    new Paragraph({
-      spacing: { before: 300 },
-      children: mainPhotos,
-    }),
-  ],
-};
+// import data from "../../../data/init.js";
+// let mainPhotos = [];
+// if (!fs.existsSync(`docxData/${data.init.numberOfDocument}/mainPhotos`))
+//   fs.mkdirSync(`docxData/${data.init.numberOfDocument}/mainPhotos`, {
+//     recursive: true,
+//   });
+// fs.readdirSync(`docxData/${data.init.numberOfDocument}/mainPhotos`).forEach(
+//   (file) => {
+//     if (file.split(".")[1] === "png") {
+//       mainPhotos.push(
+//         new ImageRun({
+//           data: fs.readFileSync(
+//             `docxData/${data.init.numberOfDocument}/mainPhotos/${file}`
+//           ),
+//           transformation: {
+//             width: 500,
+//             height: 150,
+//           },
+//         })
+//       );
+//     }
+//   }
+// );
+// console.log(mainPhotos);
+// let mainPhotosSection = {
+//   properties: { type: SectionType.NEXT_PAGE },
+//   children: [
+//     new Paragraph({
+//       alignment: AlignmentType.CENTER,
+//       spacing: { before: 200 },
+//       children: [
+//         new TextRun({
+//           size: 35,
+//           underline: { type: UnderlineType.SINGLE },
+//           text: "תמונות",
+//         }),
+//       ],
+//     }),
+//     new Paragraph({
+//       spacing: { before: 300 },
+//       children: mainPhotos,
+//     }),
+//   ],
+// };
 
 function MainPhotosSection(docxObj) {
   let mainPhotos = [];
