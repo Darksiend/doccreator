c86c27d850e6a70ea221e3ddfe7d5a8f14c180a5
Darksiend <logosmk2@gmail.com>
2023-01-05 16:42:11 +0200

    Work

--- frontend/src/pages/CreatingDocx.jsx
+++ frontend/src/pages/CreatingDocx.jsx
@@ -201,9 +201,7 @@ const CreatingDocx = () => {
     console.log(event.target.id);
     console.log(event.target.value);
     let resFloors = docxObj.floors;
-    console.log(
-      resFloors[floor.number].amydim.amydimArr[index][event.target.id]
-    );
+    console.log(resFloors[floor.number].amydim.amydimArr[index]);
     setDocxObj({ ...docxObj, floors: resFloors });
   };
 
