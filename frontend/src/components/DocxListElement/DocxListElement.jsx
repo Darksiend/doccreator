729a885116d7be6abbdfbdbfd58c932d5f125742
Darksiend <logosmk2@gmail.com>
2023-01-07 23:58:31 +0200

    Work

--- frontend/src/components/DocxListElement/DocxListElement.jsx
+++ frontend/src/components/DocxListElement/DocxListElement.jsx
@@ -24,13 +24,13 @@ const DocxListElement = (props) => {
         </p>
         <p className='documnet-info-container-text'> כתובת:{docx.address} </p>
       </div>
-      <Link className='list-btn' to={`/docx/${docx._id}`}>
-        לפתוח דוח
-      </Link>
       <img
         src={`https://docxcreateapp.onrender.com/upload/${docx.mainPhotos[0]}`}
         alt=''
       />
+      <Link className='list-btn' to={`/docx/${docx._id}`}>
+        לפתוח דוח
+      </Link>
       <button className='list-btn' onClick={onClickRemove}>
         למחוק דוח זה
       </button>
